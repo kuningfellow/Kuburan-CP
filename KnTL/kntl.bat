a71f5bb35c41b2e21b4010a1863e425ed16af7a4
unknown <williamgunawaneka@gmail.com>
2019-09-10 11:34:40 +0700

    updated windows kntl

--- KnTL/kntl.bat
+++ KnTL/kntl.bat
@@ -34,20 +34,33 @@ del %exe%
 
 :: Compiling section
 :RECOMPILE
+set compiled=1
 set tmpcpp="%cd%\.cpp"
 "%~dp0converter.exe" "%cpp%" %tmpcpp%
 if %errorlevel% neq 0 goto END
 %COMPILE% -I"%~dp0\" -o %exe% %tmpcpp%
-if %errorlevel% neq 0 goto RUN
+if %errorlevel% equ 0 goto RUN
+set compiled=0
 del %tmpcpp%
+pause > nul
 
 :: Command section
 :RUN
+cls
 set input=2
+if %compiled% equ 1 goto SOURCECOMPILED
+if %compiled% equ 0 goto SOURCENOTCOMPILED
+
+:SOURCECOMPILED
 set /p input="[92m%cpp%[0m: [93m1[0m:exe [93m2[0m:IN [93m3[0m:IN+OUT [93mp[0maste [93mc[0mompile [93mq[0muit[92m: [0m"
 if "%input%"=="1" goto LAUNCH
 if "%input%"=="2" goto LAUNCH
 if "%input%"=="3" goto LAUNCH
+goto RUN2
+:SOURCENOTCOMPILED
+set /p input="[92m%cpp%[0m: 1:exe 2:IN 3:IN+OUT [93mp[0maste [93mc[0mompile [93mq[0muit[92m: [0m"
+
+:RUN2
 if "%input%"=="q" goto END
 if "%input%"=="c" goto RECOMPILE
 if "%input%"=="p" goto PASTE
